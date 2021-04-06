5e484992252892234ae41589bec18d9a6353a5b6
Leonardo T Waterkemper <leonardo.waterkemper@bairesdev.com>
2021-04-06 15:53:32 -0300

    App creation

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+:root {
+  --dull-yellow: #efef5f;
+  --blue-green: #16775a;
+  --blue-green-two: #148060;
+  --dark-green-blue: #17644d;
+  --dark-blue-grey: #0f3328;
+  --shamrock: #02a865;
+  --green-teal: #0ebc76;
+  --white: #ffffff;
+  --warm-grey: #777777;
+  --tomato: #e12626;
+  --black: #000000;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Cousine:ital,wght@0,400;0,700;1,400;1,700&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
